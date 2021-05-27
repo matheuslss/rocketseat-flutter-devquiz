851ee9b8cf2f844b535b2e3d584ba7f3227705b7
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-26 21:46:21 -0300

    Atualizar app_widget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:nlw5_app/home/home_page.dart';
+import 'package:nlw5_app/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("NLW5 - Flutter")),
-        ),
-        body: Center(
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-              child: Text(
-                "Olá, Dev!",
-                style: TextStyle(fontSize: 30),
-              )),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'DevQuiz', home: HomePage());
   }
 }
