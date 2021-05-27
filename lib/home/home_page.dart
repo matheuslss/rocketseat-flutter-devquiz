fe6013a89800c40e3af98e497a60a03c5bc66f77
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-26 23:57:34 -0300

    Atualizar home_page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_app/home/widgets/appbar/app_bar_widget.dart';
+import 'package:nlw5_app/home/widgets/level_button/level_button_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
-    );
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LevelButtonWidget(
+                label: "Fácil",
+              ),
+              LevelButtonWidget(
+                label: "Médio",
+              ),
+              LevelButtonWidget(
+                label: "Difícil",
+              ),
+              LevelButtonWidget(
+                label: "Perito",
+              ),
+            ],
+          ),
+        ));
   }
 }
