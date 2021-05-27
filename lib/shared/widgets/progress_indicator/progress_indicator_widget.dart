abe7c9eb57e8d6605491b4ac86ed5af1a26e9845
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-27 00:49:58 -0300

    Adicionar progress_indicator_widget

---
+++ lib/shared/widgets/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nlw5_app/core/app_colors.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+  const ProgressIndicatorWidget({Key? key, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: LinearProgressIndicator(
+        value: value,
+        backgroundColor: AppColors.chartSecondary,
+        valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+      ),
+    );
+  }
+}
