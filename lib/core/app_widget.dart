f59efd23b0e725f0b4e9ef85c747e6f70f7f1895
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-27 01:23:06 -0300

    Alterar home

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:nlw5_app/challenge/challenge_page.dart';
 import 'package:nlw5_app/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'DevQuiz', home: HomePage());
+    return MaterialApp(title: 'DevQuiz', home: ChallengePage());
   }
 }
