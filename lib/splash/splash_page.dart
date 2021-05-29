2d3c7acd633af47832fcec84c4efbbfaf94b7614
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-29 19:20:14 -0300

    Atualizar splash_page

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_app/core/core.dart';
+import 'package:nlw5_app/home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage())));
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(gradient: AppGradients.linear),
