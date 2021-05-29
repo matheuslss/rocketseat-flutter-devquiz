892d475ff12e2e411dba1b3737aa3171d7c7f51c
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-29 18:13:13 -0300

    Atualizar quiz_widget

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_app/challenge/widgets/answer/answer_widget.dart';
 import 'package:nlw5_app/core/core.dart';
+import 'package:nlw5_app/shared/models/awnser_model.dart';
 import 'package:nlw5_app/shared/models/question_model.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
-  const QuizWidget({Key? key, required this.question}) : super(key: key);
+  final VoidCallback onChange;
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel anwser(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +31,24 @@ class QuizWidget extends StatelessWidget {
             height: 64,
           ),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          ...question.awnsers
-              .map((answer) => AnswerWidget(
-                    title: answer.title,
-                    isRight: answer.isRight,
-                  ))
-              .toList(),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: anwser(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((_) => widget.onChange());
+              },
+            )
         ],
       ),
     );
