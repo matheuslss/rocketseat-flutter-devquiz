32f69e8fda0d3cedf42a9d64df190ee9e5f69e51
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-28 21:27:09 -0300

    Atualizar app_bar_widget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_app/core/core.dart';
 import 'package:nlw5_app/home/widgets/score_card/score_card_widget.dart';
+import 'package:nlw5_app/shared/models/user_model.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+
+  AppBarWidget({required this.user})
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
@@ -23,7 +26,7 @@ class AppBarWidget extends PreferredSize {
                             style: AppTextStyles.title,
                             children: [
                               TextSpan(
-                                  text: "Matheus",
+                                  text: user.name,
                                   style: AppTextStyles.titleBold)
                             ])),
                         Container(
@@ -32,8 +35,7 @@ class AppBarWidget extends PreferredSize {
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
-                                  image: NetworkImage(
-                                      "https://avatars.githubusercontent.com/u/47352591?v=4"))),
+                                  image: NetworkImage(user.photoUrl))),
                         )
                       ],
                     ),
