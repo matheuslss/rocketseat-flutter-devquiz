d41be580046f0734e92ae383d41e4dffc8086326
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-28 22:42:35 -0300

    Corrigir nomenclatura

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nlw5_app/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:nlw5_app/challenge/widgets/answer/answer_widget.dart';
 import 'package:nlw5_app/core/core.dart';
 
 class QuizWidget extends StatelessWidget {
@@ -18,21 +18,21 @@ class QuizWidget extends StatelessWidget {
           SizedBox(
             height: 24,
           ),
-          AwnserWidget(
+          AnswerWidget(
             title:
                 "Possibilita a criação de aplicativos compilados nativamente",
           ),
-          AwnserWidget(
+          AnswerWidget(
             title:
                 "Possibilita a criação de aplicativos compilados nativamente",
             isRight: true,
             isSelect: true,
           ),
-          AwnserWidget(
+          AnswerWidget(
             title:
                 "Possibilita a criação de aplicativos compilados nativamente",
           ),
-          AwnserWidget(
+          AnswerWidget(
             title:
                 "Possibilita a criação de aplicativos compilados nativamente",
             isSelect: true,
