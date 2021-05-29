29b286d72ba538af84556cac70ef367d36759824
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-29 19:22:37 -0300

    Atualizar next_button_widget

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -26,11 +26,30 @@ class NextButtonWidget extends StatelessWidget {
         this.label = label,
         this.borderColor = AppColors.darkGreen,
         this.onTap = onTap;
+
   NextButtonWidget.white({required String label, required VoidCallback onTap})
       : this.backgroundColor = AppColors.white,
         this.fontColor = AppColors.grey,
         this.label = label,
         this.borderColor = AppColors.border,
+        this.onTap = onTap;
+
+  NextButtonWidget.purple({
+    required String label,
+    required VoidCallback onTap,
+  })  : this.backgroundColor = AppColors.purple,
+        this.fontColor = AppColors.white,
+        this.label = label,
+        this.borderColor = AppColors.purple,
+        this.onTap = onTap;
+
+  NextButtonWidget.transparent({
+    required String label,
+    required VoidCallback onTap,
+  })  : this.backgroundColor = Colors.transparent,
+        this.fontColor = AppColors.grey,
+        this.label = label,
+        this.borderColor = Colors.transparent,
         this.onTap = onTap;
 
   @override
