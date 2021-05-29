5d71a98cb01dea05886aa858783f82c0562a61f6
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-29 00:23:20 -0300

    Atualizar question_indicator_widget

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:nlw5_app/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
-  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+  final int currentPage;
+  final int length;
+  const QuestionIndicatorWidget(
+      {Key? key, required this.currentPage, required this.length})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +16,12 @@ class QuestionIndicatorWidget extends StatelessWidget {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text("Questão 04"), Text("de 10")],
+            children: [Text("Questão ${currentPage}"), Text("de ${length}")],
           ),
           SizedBox(
             height: 16,
           ),
-          ProgressIndicatorWidget(value: 0.7)
+          ProgressIndicatorWidget(value: currentPage / length)
         ],
       ),
     );
