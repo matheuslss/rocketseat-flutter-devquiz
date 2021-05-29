73c9a6937ebff954978bad0275298b6d55e7937e
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-29 19:23:36 -0300

    Atualizar app_widget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:nlw5_app/challenge/challenge_page.dart';
-import 'package:nlw5_app/home/home_page.dart';
+import 'package:nlw5_app/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'DevQuiz', home: HomePage());
+    return MaterialApp(title: 'DevQuiz', home: SplashPage());
   }
 }
