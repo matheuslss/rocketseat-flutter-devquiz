01859b705e51b744088e765fd3883fff2ba03e2b
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-05-28 21:39:14 -0300

    Adicionar gerencia de estado

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:nlw5_app/core/core.dart';
 import 'package:nlw5_app/home/home_state.dart';
 import 'package:nlw5_app/shared/models/awnser_model.dart';
@@ -6,14 +7,17 @@ import 'package:nlw5_app/shared/models/quiz_model.dart';
 import 'package:nlw5_app/shared/models/user_model.dart';
 
 class HomeController {
-  HomeState state = HomeState.empty;
+  ValueNotifier<HomeState> stateNotifier =
+      ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
 
   UserModel? user;
   List<QuizModel>? quizzes;
 
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 1));
     user = UserModel(
         name: "Matheus",
         photoUrl: "https://avatars.githubusercontent.com/u/47352591?v=4");
@@ -23,7 +27,7 @@ class HomeController {
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 1));
     quizzes = [
       QuizModel(
           title: "NLW 5 Flutter",
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nlw5_app/core/core.dart';
+import 'package:nlw5_app/home/home_controller.dart';
+import 'package:nlw5_app/home/home_state.dart';
 import 'package:nlw5_app/home/widgets/appbar/app_bar_widget.dart';
 import 'package:nlw5_app/home/widgets/level_button/level_button_widget.dart';
 import 'package:nlw5_app/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -11,52 +14,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBarWidget(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return controller.state == HomeState.success
+        ? Scaffold(
+            appBar: AppBarWidget(
+              user: controller.user!,
+            ),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
                 children: [
-                  LevelButtonWidget(
-                    label: "Fácil",
+                  SizedBox(
+                    height: 24,
                   ),
-                  LevelButtonWidget(
-                    label: "Médio",
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      LevelButtonWidget(
+                        label: "Fácil",
+                      ),
+                      LevelButtonWidget(
+                        label: "Médio",
+                      ),
+                      LevelButtonWidget(
+                        label: "Difícil",
+                      ),
+                      LevelButtonWidget(
+                        label: "Perito",
+                      ),
+                    ],
                   ),
-                  LevelButtonWidget(
-                    label: "Difícil",
+                  SizedBox(
+                    height: 24,
                   ),
-                  LevelButtonWidget(
-                    label: "Perito",
-                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      children: controller.quizzes!
+                          .map((quiz) => QuizCardWidget(
+                                title: quiz.title,
+                                completed:
+                                    "${quiz.quetionAwnsered} de ${quiz.questions.length}",
+                                percent: quiz.quetionAwnsered /
+                                    quiz.questions.length,
+                                onTap: () {},
+                              ))
+                          .toList(),
+                    ),
+                  )
                 ],
               ),
-              SizedBox(
-                height: 24,
+            ))
+        : Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
               ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  children: [
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+            ),
+          );
   }
 }
